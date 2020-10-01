2c1ac374bfac35581d3073d065b981f0cea0ce6f
Cheng <chengjohnsonhs@gmail.com>
2020-10-02 05:21:47 +0800

    Updates

---
+++ precache-manifest.4f8fb7142e4692a642a9841fb9555a0a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "022af7ec07a54e85a7ce9da6fcd6e49d",
+    "url": "/apple-clone/index.html"
+  },
+  {
+    "revision": "4f5f933f2ceaf0922cde",
+    "url": "/apple-clone/static/css/main.1a5aa1c6.chunk.css"
+  },
+  {
+    "revision": "f50d91b9f1128d4f3b79",
+    "url": "/apple-clone/static/js/2.d9154bda.chunk.js"
+  },
+  {
+    "revision": "d4b0302a1e882c57090284dfea2c6f01",
+    "url": "/apple-clone/static/js/2.d9154bda.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f5f933f2ceaf0922cde",
+    "url": "/apple-clone/static/js/main.7d9e361f.chunk.js"
+  },
+  {
+    "revision": "7bb73c959775184339fe",
+    "url": "/apple-clone/static/js/runtime-main.775e4c48.js"
+  }
+]);
