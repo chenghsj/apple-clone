ebe313a0fbc6a25b722abf7d70f5179c9d017c03
Cheng <chengjohnsonhs@gmail.com>
2020-10-06 03:55:33 +0800

    💫 UPDATE: add retina intro

--- src/components/MacbookRetina.jsx
+++ src/components/MacbookRetina.jsx
@@ -11,6 +11,7 @@ function MacbookRetina() {
   let macbookImg = useRef(null);
   let proQuote = useRef(null);
   let proName = useRef(null);
+  let retinaIntro = useRef(null);
 
   useEffect(() => {
     let hardwareRatio = window.innerWidth / (1136 - 248);
@@ -42,8 +43,8 @@ function MacbookRetina() {
         start: "top top",
         end: "bottom top",
         pin: true,
-        markers: true,
         pinReparent: true,
+        // markers: true,
       },
     });
     hardwareBlendAnim
@@ -60,14 +61,25 @@ function MacbookRetina() {
         },
         {
           clipPath: "polygon(0% 0%, 100% 0%, 100% 0%, 0% 0%)",
-          duration: 3,
+          duration: 2,
           delay: 0.5,
         }
       )
       .to(proName, { opacity: 0, duration: 0.1 }, "<1")
       .fromTo(proQuote, { opacity: 0 }, { opacity: 1, duration: 0.15 }, "<0.8")
-      .to(macbookImg, { scale: 1, delay: 0.5 })
-      .to(proQuote, { scale: 0.8, opacity: 0, duration: 0.3 }, "<");
+      .to(proQuote, { scale: 0.8, opacity: 0, duration: 0.3, delay: 0.5 })
+      .to(macbookImg, { scale: 1, top: "50%" }, "<");
+
+    let introAnim = gsap.timeline({
+      scrollTrigger: {
+        trigger: retinaIntro,
+        start: "top bottom-=3%",
+        end: "bottom bottom",
+        scrub: 0.1,
+        // markers: true,
+      },
+    });
+    introAnim.fromTo(retinaIntro, { opacity: 0 }, { opacity: 1 });
   }, []);
 
   return (
@@ -131,6 +143,20 @@ function MacbookRetina() {
                 <figure className="figure-image from-image"></figure>
               </div>
             </div>
+          </div>
+          <div className="section-content">
+            <p
+              ref={(el) => (retinaIntro = el)}
+              className="typography-section-intro"
+            >
+              全新 MacBook Pro 配備絢麗的
+              <span className="bold"> 16&nbsp;吋 Retina 顯示器</span>，這是迄今
+              Mac 筆記型電腦中最大的 Retina 顯示器。它所產生的{" "}
+              <span className="bold">500 尼特亮度</span>
+              ，可實現精彩奪目的亮部和明亮的白色；得益於液晶分子精準的光配向技術，深邃的黑色也能同時展現。
+              <span className="bold">P3 廣色域</span>
+              造就亮麗鮮活的影像與影片。因此，無論你身在何處，你的作品都能以最優的光感與色彩呈現眼前。
+            </p>
           </div>
         </div>
       </div>
